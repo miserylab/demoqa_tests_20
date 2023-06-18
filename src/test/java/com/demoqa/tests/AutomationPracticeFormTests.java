aefa12062943ec3cf21a0792000d283b2c51b395
Olga Demina <misertylab.r6s@gmail.com>
2023-06-18 22:02:54 +0300

    added homework 7 page_objects

--- src/test/java/com/demoqa/tests/AutomationPracticeFormTests.java
+++ src/test/java/com/demoqa/tests/AutomationPracticeFormTests.java
@@ -46,26 +46,5 @@ public class AutomationPracticeFormTests extends TestBase {
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text("Alex"), text("Egorov"), text("alex@egorov.com"),
                 text("0123456789"));
-//        $(".modal-dialog tr:nth-child(1) td:nth-child(1)").shouldHave(text("Student Name"));
-//        $(".modal-dialog tr:nth-child(1) td:nth-child(2)").shouldHave(text("Alex Egorov"));
-//        $(".modal-dialog tr:nth-child(2) td:nth-child(1)").shouldHave(text("Student Email"));
-//        $(".modal-dialog tr:nth-child(2) td:nth-child(2)").shouldHave(text("alex@egorov.com"));
-//        $(".modal-dialog tr:nth-child(3) td:nth-child(1)").shouldHave(text("Gender"));
-//        $(".modal-dialog tr:nth-child(3) td:nth-child(2)").shouldHave(text("Male"));
-//        $(".modal-dialog tr:nth-child(4) td:nth-child(1)").shouldHave(text("Mobile"));
-//        $(".modal-dialog tr:nth-child(4) td:nth-child(2)").shouldHave(text("0123456789"));
-//        $(".modal-dialog tr:nth-child(5) td:nth-child(1)").shouldHave(text("Date of Birth"));
-//        $(".modal-dialog tr:nth-child(5) td:nth-child(2)").shouldHave(text("18 July,2003"));
-//        $(".modal-dialog tr:nth-child(6) td:nth-child(1)").shouldHave(text("Subjects"));
-//        $(".modal-dialog tr:nth-child(6) td:nth-child(2)").shouldHave(text("Chemistry, Physics"));
-//        $(".modal-dialog tr:nth-child(7) td:nth-child(1)").shouldHave(text("Hobbies"));
-//        $(".modal-dialog tr:nth-child(7) td:nth-child(2)").shouldHave(text("Sports, Reading"));
-//        $(".modal-dialog tr:nth-child(8) td:nth-child(1)").shouldHave(text("Picture"));
-//        $(".modal-dialog tr:nth-child(8) td:nth-child(2)").shouldHave(text("Toolsqa.jpg"));
-//        $(".modal-dialog tr:nth-child(9) td:nth-child(1)").shouldHave(text("Address"));
-//        $(".modal-dialog tr:nth-child(9) td:nth-child(2)").shouldHave(text("Some address 1"));
-//        $(".modal-dialog tr:nth-child(10) td:nth-child(1)").shouldHave(text("State and City"));
-//        $(".modal-dialog tr:nth-child(10) td:nth-child(2)").shouldHave(text("Uttar Pradesh Merrut"));
-//        $(".modal-dialog #closeLargeModal").shouldHave(text("Close"));
     }
 }
