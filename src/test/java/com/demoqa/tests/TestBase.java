6bdc400cb38ee016b6873fa905bae2f764db6bef
Olga Demina <misertylab.r6s@gmail.com>
2023-07-16 20:06:41 +0300

    added static testdata

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -3,7 +3,17 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
+
+
+
+
 public class TestBase {
+
+//        String firstName = "Alex",
+//            lastName = "Egorov",
+//            userEmail = "alex@egorov.com";
+
+
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
